c355598aaed89f492dd298527b525ed57086e413
nourb <nourb@LAPTOP-484H9GNM>
2023-12-07 17:41:49 +0100

    Ajoutez des fichiers projet.

---
+++ 222222222/Form1.Designer.vb
@@ -0,0 +1,274 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.coff_a = New System.Windows.Forms.TextBox()
+        Me.coff_b = New System.Windows.Forms.TextBox()
+        Me.coff_c = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.lbl_delta = New System.Windows.Forms.Label()
+        Me.lbl_result2 = New System.Windows.Forms.Label()
+        Me.lbl_result3x1 = New System.Windows.Forms.Label()
+        Me.lbl_result3x2 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.lbl_result1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Tai Le", 10.2!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(98, 78)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(58, 22)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "coff_a"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Tai Le", 10.2!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(308, 78)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(60, 22)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "coff_b"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Tai Le", 10.2!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(572, 78)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(57, 22)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "coff_c"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Tai Le", 10.2!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(253, 245)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(51, 22)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "delta"
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(312, 163)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(141, 31)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "RESOUDRE"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'coff_a
+        '
+        Me.coff_a.Location = New System.Drawing.Point(192, 78)
+        Me.coff_a.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.coff_a.Name = "coff_a"
+        Me.coff_a.Size = New System.Drawing.Size(100, 22)
+        Me.coff_a.TabIndex = 5
+        '
+        'coff_b
+        '
+        Me.coff_b.Location = New System.Drawing.Point(400, 78)
+        Me.coff_b.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.coff_b.Name = "coff_b"
+        Me.coff_b.Size = New System.Drawing.Size(100, 22)
+        Me.coff_b.TabIndex = 6
+        '
+        'coff_c
+        '
+        Me.coff_c.Location = New System.Drawing.Point(653, 78)
+        Me.coff_c.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.coff_c.Name = "coff_c"
+        Me.coff_c.Size = New System.Drawing.Size(100, 22)
+        Me.coff_c.TabIndex = 7
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Tai Le", 10.2!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(189, 318)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(44, 22)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "cas1"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Tai Le", 10.2!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(189, 354)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(44, 22)
+        Me.Label6.TabIndex = 10
+        Me.Label6.Text = "cas2"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Tai Le", 10.2!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(189, 388)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(44, 22)
+        Me.Label7.TabIndex = 11
+        Me.Label7.Text = "cas3"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(0, 0)
+        Me.Label8.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(51, 17)
+        Me.Label8.TabIndex = 12
+        Me.Label8.Text = "Label8"
+        '
+        'lbl_delta
+        '
+        Me.lbl_delta.Font = New System.Drawing.Font("Microsoft Tai Le", 10.2!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl_delta.Location = New System.Drawing.Point(360, 245)
+        Me.lbl_delta.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lbl_delta.Name = "lbl_delta"
+        Me.lbl_delta.Size = New System.Drawing.Size(133, 28)
+        Me.lbl_delta.TabIndex = 13
+        Me.lbl_delta.Text = " "
+        '
+        'lbl_result2
+        '
+        Me.lbl_result2.AutoSize = True
+        Me.lbl_result2.Font = New System.Drawing.Font("Microsoft Tai Le", 10.2!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl_result2.Location = New System.Drawing.Point(348, 354)
+        Me.lbl_result2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lbl_result2.Name = "lbl_result2"
+        Me.lbl_result2.Size = New System.Drawing.Size(15, 22)
+        Me.lbl_result2.TabIndex = 14
+        Me.lbl_result2.Text = " "
+        '
+        'lbl_result3x1
+        '
+        Me.lbl_result3x1.AutoSize = True
+        Me.lbl_result3x1.Font = New System.Drawing.Font("Microsoft Tai Le", 10.2!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl_result3x1.Location = New System.Drawing.Point(348, 388)
+        Me.lbl_result3x1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lbl_result3x1.Name = "lbl_result3x1"
+        Me.lbl_result3x1.Size = New System.Drawing.Size(15, 22)
+        Me.lbl_result3x1.TabIndex = 15
+        Me.lbl_result3x1.Text = " "
+        '
+        'lbl_result3x2
+        '
+        Me.lbl_result3x2.AutoSize = True
+        Me.lbl_result3x2.Font = New System.Drawing.Font("Microsoft Tai Le", 10.2!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl_result3x2.Location = New System.Drawing.Point(488, 388)
+        Me.lbl_result3x2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lbl_result3x2.Name = "lbl_result3x2"
+        Me.lbl_result3x2.Size = New System.Drawing.Size(15, 22)
+        Me.lbl_result3x2.TabIndex = 16
+        Me.lbl_result3x2.Text = " "
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Font = New System.Drawing.Font("Microsoft Tai Le", 10.2!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label9.Location = New System.Drawing.Point(300, 244)
+        Me.Label9.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(27, 22)
+        Me.Label9.TabIndex = 17
+        Me.Label9.Text = " ="
+        '
+        'lbl_result1
+        '
+        Me.lbl_result1.AutoSize = True
+        Me.lbl_result1.Font = New System.Drawing.Font("Microsoft Tai Le", 10.2!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl_result1.Location = New System.Drawing.Point(348, 318)
+        Me.lbl_result1.Name = "lbl_result1"
+        Me.lbl_result1.Size = New System.Drawing.Size(15, 22)
+        Me.lbl_result1.TabIndex = 18
+        Me.lbl_result1.Text = " "
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.LightSalmon
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.lbl_result1)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.lbl_result3x2)
+        Me.Controls.Add(Me.lbl_result3x1)
+        Me.Controls.Add(Me.lbl_result2)
+        Me.Controls.Add(Me.lbl_delta)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.coff_c)
+        Me.Controls.Add(Me.coff_b)
+        Me.Controls.Add(Me.coff_a)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents coff_a As TextBox
+    Friend WithEvents coff_b As TextBox
+    Friend WithEvents coff_c As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents lbl_delta As Label
+    Friend WithEvents lbl_result2 As Label
+    Friend WithEvents lbl_result3x1 As Label
+    Friend WithEvents lbl_result3x2 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents lbl_result1 As Label
+End Class
---
+++ 222222222/Form1.vb
@@ -0,0 +1,58 @@
+﻿Public Class Form1
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim a As Single
+        Dim b As Single
+        Dim c As Single
+        a = CSng(coff_a.Text)
+        b = CSng(coff_b.Text)
+        c = CSng(coff_c.Text)
+        Dim delta As Single
+        delta = (b * b) - 4 * (a * c)
+        lbl_delta.Text = delta.ToString
+        If delta < 0 Then
+            MsgBox("nb solution = 0")
+            lbl_result1.Text = "pas de solution"
+            lbl_result3x1.Text = " "
+            lbl_result3x2.Text = " "
+            lbl_result2.Text = " "
+        Else
+            If delta = 0 Then
+                Dim x12 As Single
+                x12 = (-b) / (2 * a)
+                MsgBox("nb solution = 1")
+                lbl_result2.Text = x12.ToString
+                lbl_result3x1.Text = " "
+                lbl_result3x2.Text = " "
+            Else
+                Dim x1 As Single
+                Dim x2 As Single
+                x1 = (-b) / (2 * a)
+                x2 = b / (2 * a)
+                MsgBox("nb solution = 2")
+                lbl_result3x1.Text = x1.ToString
+                lbl_result3x2.Text = x2.ToString
+                lbl_result1.Text = " "
+                lbl_result2.Text = " "
+
+
+
+            End If
+        End If
+    End Sub
+
+    Private Sub lbl_delta_Click(sender As Object, e As EventArgs) Handles lbl_delta.Click
+
+    End Sub
+End Class
